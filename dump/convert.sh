c4c62ec50390f328ad2884a9b5b561ecf9bf0d12
Dennis Tobola <Dennis.tobola@gbv.de>
2019-03-12 09:42:05 +0100

    convert.sh und download.sh für GeoNames-Konvertierung zu JSKOS

---
+++ dump/convert.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Dieses Skript erstellt eine Datei languagecodes.txt, zu der die ersten beiden Großbuchstaben (AD.zip, AE.zip, ...) aus files.txt extrahiert werden. Dann wird languagecodes.txt line-for-line gelesen und die ZIP-Dateien im dump zu NDJSON-Dateien konvertiert.
+
+grep -o '[A-Z][A-Z]' files.txt > languagecodes.txt
+
+while IFS='' read -r code || [[ -n "$code" ]]; do
+    ../geonames.js $code.zip > $code.ndjson
+	echo $code.ndjson
+done < "languagecodes.txt"
+
+
+
+
--- dump/download.sh
+++ dump/download.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#Dieses Skript lädt die ZIP-Dateien der GeoNames-Länder herunter und speichert sie, die Extra-Dateien hierarchie.zip und no-country.zip wurden hinzugefügt.
+#Dieses Skript lädt die ZIP-Dateien der GeoNames-Länder herunter und speichert sie, die Extra-Dateien hierarchie.zip und no-country.zip wurden hinzugefügt. XX.zip existiert nicht(?), daher wird sie nicht mitbeachtet
 
-curl -s http://download.geonames.org/export/dump/ | grep -o '[A-Z][A-Z]\.zip' > files.txt
+curl -s http://download.geonames.org/export/dump/ | grep -o '[A-Z][A-Z]\.zip' | grep -v '[X][X]\.zip' | uniq > files.txt
 echo hierarchy.zip >> files.txt
 echo no-country.zip >> files.txt
 
